5774428bf6cdaab9c019b48ef384fc70b3dfbf14
真琴 高田 <makoto-r.3277@outlook.jp>
2017-12-15 17:29:46 +0900

    ビット演算子を扱う問題1

--- 組み込み　演習/組み込み　演習/演習2_3.cpp
+++ 組み込み　演習/組み込み　演習/演習2_3.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <limits.h>
 
-int main(void) {
+int main(void)
+{
+	unsigned int x, y, z;
 
-	int a;
-	int b;
+	printf("0～%uの整数を２つ入力\n", UINT_MAX);
+	scanf_s("%u%u", &x, &y);
 
-	printf("Please input (>0) : ");
-	scanf_s("%d", &a);
+	z = x & y;
+	printf("%u(%#x) AND %u(%#x) = %#x\n", x, x, y, y, z);
 
-	if (a < 1) {
-		printf("Bad input\n");
-	}
-	else {
-		b = a * 2;
-		printf("Your input value is %d, new value is %d\n", a, b);
-	}
+	z = x | y;
+	printf("%u(%#x) OR  %u(%#x) = %#x\n", x, x, y, y, z);
+
+	z = x ^ y;
+	printf("%u(%#x) XOR %u(%#x) = %#x\n", x, x, y, y, z);
 
 	return 0;
 }
+//  %u は、unsigned型に対する書式指定文字列。
+//  UINT_MAX は limits.h の中でマクロ定義され、unsigned int型の最大値を表す。
