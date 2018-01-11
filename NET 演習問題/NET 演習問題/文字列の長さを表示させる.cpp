1170085fa714a4026fd2a50853fb8716b2c6f1bd
真琴 高田 <makoto-r.3277@outlook.jp>
2018-01-11 15:08:56 +0900

    ポインタ

---
+++ NET 演習問題/NET 演習問題/文字列の長さを表示させる.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	char name[] = "Wakaba-ku";
+	char *p;
+	int n = 0;
+
+	for (p = name; *p != '\0'; p++) {
+
+		n++;
+
+	}
+
+	printf("n = %d\n", n);
+
+	return 0;
+}
