eee12575ff1bf2756bf98b023188b3d0f2847cbe
真琴 高田 <makoto-r.3277@outlook.jp>
2018-01-11 14:21:13 +0900

    ポインタ

---
+++ NET 演習問題/NET 演習問題/ポインタ変数のサイズ確認.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+	char   a, *pa;
+	int    b, *pb;
+	float  c, *pc;
+	double d, *pd;
+
+	printf("size of a  = %d byte\n", sizeof(a));
+	printf("size of pa = %d byte\n", sizeof(pa));
+
+	printf("size of b  = %d byte\n", sizeof(b));
+	printf("size of pb = %d byte\n", sizeof(pb));
+
+	printf("size of c  = %d byte\n", sizeof(c));
+	printf("size of pc = %d byte\n", sizeof(pc));
+
+	printf("size of d  = %d byte\n", sizeof(d));
+	printf("size of pd = %d byte\n", sizeof(pd));
+
+	return 0;
+}
